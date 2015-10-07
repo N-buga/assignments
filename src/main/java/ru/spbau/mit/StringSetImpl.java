3677e1c957eb5eee6aa3419dd425a74a6e0ff8f2
N-buga <bugakova_nadya@mail.ru>
2015-10-07 13:36:43 +0300

    fix

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,23 +1,18 @@
 package ru.spbau.mit;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-
 /**
  * Created by n_buga on 22.09.15.
  */
-
 public class StringSetImpl implements StreamSerializable, StringSet {
     private static class Vertex {
-        private int termVertexLower;
-        private boolean termVertex;
+        private int countTermVertexLower;
+        private boolean isVertex;
         private Vertex[] links = new Vertex[256];
     }
-
-    private static final int ENDOFFILE = 9;
-
+    private static final int END_OF_FILE = 9;
     @Override
     public void serialize(OutputStream out) throws SerializationException {
         try {
@@ -26,19 +21,17 @@ public class StringSetImpl implements StreamSerializable, StringSet {
             throw new SerializationException("serialize");
         }
     }
-
     private void doSerialize(OutputStream out) throws IOException {
         Vertex curVertex = vertexHead;
         StringBuilder curString = new StringBuilder();
         goRoundTree(out, curVertex, curString);
-        out.write((char) ENDOFFILE);
+        out.write((char) END_OF_FILE);
     }
-
     private void goRoundTree(OutputStream out, Vertex curVertex, StringBuilder curString) throws IOException {
         if (curVertex == null) {
             return;
         }
-        if (curVertex.termVertex) {
+        if (curVertex.isVertex) {
             for (int i = 0; i < curString.length(); i++) {
                 out.write(curString.charAt(i));
             }
@@ -52,7 +45,6 @@ public class StringSetImpl implements StreamSerializable, StringSet {
             }
         }
     }
-
     @Override
     public void deserialize(InputStream in) throws SerializationException{
         try {
@@ -61,26 +53,29 @@ public class StringSetImpl implements StreamSerializable, StringSet {
             throw new SerializationException("deserialize");
         }
     }
-
     private void doDeserialize(InputStream in) throws IOException {
         vertexHead = new Vertex();
         char c = 0;
         int i = 0;
-        while (i != ENDOFFILE) {
+        while (i != END_OF_FILE) {
             StringBuilder curString = new StringBuilder();
             i = in.read();
-            while ((c = (char)i) != '\n' && i != ENDOFFILE) {
+            while ((c = (char)i) != '\n' && i != END_OF_FILE) {
                 curString.append(c);
                 i = in.read();
             }
-            if (i == ENDOFFILE) {
+            if (i == END_OF_FILE) {
                 break;
             }
             add(curString.toString());
         }
     }
-
     private Vertex vertexHead = new Vertex();
+    private Vertex nextForAdd(Vertex curVertex, char c) {
+        curVertex =  curVertex.links[c];
+        curVertex.countTermVertexLower++;
+        return curVertex;
+    }
 
     @Override
     public boolean add(String element){
@@ -88,73 +83,113 @@ public class StringSetImpl implements StreamSerializable, StringSet {
             return false;
         }
         Vertex currentVertex = vertexHead;
-        currentVertex.termVertexLower++;
+        currentVertex.countTermVertexLower++;
         for (char c: element.toCharArray()){
             if (currentVertex.links[(int)c] != null) {
-                currentVertex = currentVertex.links[c];
-                currentVertex.termVertexLower++;
+                currentVertex = nextForAdd(currentVertex, c);
             }
             else {
                 Vertex newVertex = new Vertex();
                 currentVertex.links[(int)c] = newVertex;
-                currentVertex = currentVertex.links[c];
-                currentVertex.termVertexLower++;
+                currentVertex = nextForAdd(currentVertex, c);
             }
         }
-        currentVertex.termVertex = true;
+        currentVertex.isVertex = true;
         return true;
     }
-
     @Override
     public boolean contains(String element) {
         Vertex curVertex = vertexHead;
-        int i;
-        for (i = 0; i < element.length(); i++) {
-            if (curVertex.links[element.charAt(i)] == null) {
+        for (char c: element.toCharArray()) {
+            if (curVertex.links[c] == null) {
                 return false;
             }
-            curVertex = curVertex.links[element.charAt(i)];
+            curVertex = curVertex.links[c];
         }
-        return curVertex.termVertex;
+        return curVertex.isVertex;
+    }
+
+    private int helperRemove_Prefix(String element, String function) {
+        Vertex curVertex = vertexHead;
+        for (char c: element.toCharArray()){
+            if (curVertex.links[c] == null) {
+                return 0;
+            }
+            if (curVertex.countTermVertexLower > 1 && curVertex.links[c].countTermVertexLower == 1) {
+                if (function == "remove") {
+                    curVertex.countTermVertexLower--;
+                    curVertex.links[c] = null;
+                }
+                return 1;
+            }
+            else {
+                if (function == "remove") {
+                    curVertex.countTermVertexLower--;
+                }
+                curVertex = curVertex.links[c];
+            }
+        }
+        if (function == "remove") {
+            if (curVertex.isVertex) {
+                curVertex.isVertex = false;
+                curVertex.countTermVertexLower--;
+            } else {
+                return 0;
+            }
+        }
+        return curVertex.countTermVertexLower;
     }
 
     @Override
     public boolean remove(String element) {
-        if (!this.contains(element)) {
+        if (helperRemove_Prefix(element, "remove") == 0) {
             return false;
+        } else {
+            return true;
         }
-        Vertex curVertex = vertexHead;
-        for (int i = 0; i < element.length(); i++){
-            if (curVertex.termVertexLower > 1 && curVertex.links[element.charAt(i)].termVertexLower == 1) {
-                curVertex.termVertexLower--;
-                curVertex.links[element.charAt(i)] = null;
-                return true;
-            }
-            else {
-                curVertex.termVertexLower--;
-                curVertex = curVertex.links[element.charAt(i)];
-            }
-        }
-        curVertex.termVertex = false;
-        curVertex.termVertexLower--;
-        return true;
-    }
-
-    @Override
-    public int size() {
-        return vertexHead.termVertexLower;
     }
 
     @Override
     public int howManyStartsWithPrefix(String prefix) {
-        int i;
+        return helperRemove_Prefix(prefix, "howManyStartsWithPrefix");
+    }
+
+    /*    @Override
+            public boolean remove(String element) {
+                if (!contains(element)) {
+                    return false;
+                }
+                Vertex curVertex = vertexHead;
+                for (char c: element.toCharArray()){
+                    if (curVertex.countTermVertexLower > 1 && curVertex.links[c].countTermVertexLower == 1) {
+                        curVertex.countTermVertexLower--;
+                        curVertex.links[c] = null;
+                        return true;
+                    }
+                    else {
+                        curVertex.countTermVertexLower--;
+                        curVertex = curVertex.links[c];
+                    }
+                }
+                curVertex.isVertex = false;
+                curVertex.countTermVertexLower--;
+                return true;
+            }
+        */
+    @Override
+    public int size() {
+        return vertexHead.countTermVertexLower;
+    }
+/*    @Override
+    public int howManyStartsWithPrefix(String prefix) {
         Vertex curVertex = vertexHead;
-        for (i = 0; i < prefix.length(); i++) {
-            if (curVertex.links[prefix.charAt(i)] == null) {
+        for (char c: prefix.toCharArray()) {
+            if (curVertex.links[c] == null) {
                 return 0;
             }
-            curVertex = curVertex.links[prefix.charAt(i)];
+            curVertex = curVertex.links[c];
         }
-        return curVertex.termVertexLower;
+        return curVertex.countTermVertexLower;
     }
+*/
 }
