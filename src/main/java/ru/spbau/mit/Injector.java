0ca0075f575a0fe43b4d8f7075f18edc3b4ffbdb
N-buga <bugakova_nadya@mail.ru>
2015-12-03 01:16:39 +0300

    t

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -68,11 +68,15 @@ public class Injector {
 
         isTake.put(rootClass, false);
         try {
+            if (instancesClasses.containsKey(rootClass)) {
+                return instancesClasses.get(rootClass);
+            }
             Object returnValue = rootConstructor.newInstance(args.toArray());
             instancesClasses.put(rootClass, returnValue);
             return returnValue;
         } catch (InvocationTargetException e) {
-            throw new AmbiguousImplementationException();
+//            throw new AmbiguousImplementationException();
+            throw e;
         }
     }
 }
